91c903275931103672ab8887a5f1c52e77ba8370
marcolamas <itservice_practicas@metricamovil.com>
2025-10-10 15:28:11 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -73,7 +73,9 @@ SHEET_TABLE = "Equipos"
 def load_data():
     try:
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive.readonly"]
-        creds = ServiceAccountCredentials.from_json_keyfile_name("credenciales.json", scope)
+        creds_dict = st.secrets["google_credentials"]
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
         client = gspread.authorize(creds)
         
         spreadsheet = client.open_by_url(SPREADSHEET_URL)
