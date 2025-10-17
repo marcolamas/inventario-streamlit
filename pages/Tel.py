07a95876eeabd91fba55b07b51c0d742f0c57c4e
marcolamas <itservice_practicas@metricamovil.com>
2025-10-17 09:30:44 -0600

    Update Tel.py

--- pages/Tel.py
+++ pages/Tel.py
@@ -74,6 +74,7 @@ SHEET_GAUGES = "Web"
 @st.cache_data(ttl=300)
 def load_all_data():
     try:
+        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive.readonly"]
         creds_dict = st.secrets["gcp_service_account"]
         creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
         client = gspread.authorize(creds)
@@ -348,5 +349,6 @@ AgGrid(
     data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
     custom_css=custom_css
 )
+
 
 
